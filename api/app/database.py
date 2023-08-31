5b20f5d2e2f8c421524b1e66434c9e58b5454d33
Xukay101 <zaca03zaca@gmail.com>
2023-08-31 19:00:58 -0300

    Refactored database connection and controller structure

--- api/app/database.py
+++ api/app/database.py
@@ -1,51 +1,81 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from typing import List
+
+from pydantic import BaseModel
 from bson import ObjectId
+from motor.motor_asyncio import (
+    AsyncIOMotorClient, 
+    AsyncIOMotorCollection, 
+    AsyncIOMotorCursor
+)
 
 from app.config import settings
-from app.models import User
+from app.models import User, Question, Answer, Tag
 
 # Conections
 client = AsyncIOMotorClient(settings.MONGODB_URI)
 database = client[settings.MONGODB_NAME]
 
 # Controllers
-class UserController():
+class BaseController:
+    model: BaseModel
+    collection: AsyncIOMotorCollection
+
+    @classmethod
+    async def get_by_id(cls, id: str) -> dict:
+        document = await cls.collection.find_one({'_id': ObjectId(id)})
+        return document
+
+    @classmethod
+    async def get_all(cls) -> List[BaseModel]:
+        documents = []
+        cursor: AsyncIOMotorCursor = cls.collection.find({})
+        async for doc in cursor:
+            documents.append(cls.model(**doc))
+        return documents
+
+    @classmethod
+    async def create(cls, data: dict) -> dict:
+        new_document = await cls.collection.insert_one(data)
+        created_document = await cls.collection.find_one({'_id': new_document.inserted_id})
+        created_document['_id'] = str(created_document['_id'])
+        return created_document
+
+    @classmethod
+    async def update(cls, id: str, data: dict) -> dict:
+        await cls.collection.update_one({'_id': id}, {'$set': data})
+        document = await cls.collection.find_one({'_id': id})
+        return document
+
+    @classmethod
+    async def delete(cls, id: str) -> bool:
+        result = await cls.collection.delete_one({'_id': id})
+        return result.deleted_count > 0
+
+class UserController(BaseController):
+    model = User
     collection = database['users']
 
     @classmethod
-    async def get_by_id(cls, id: str):
-        user = await cls.collection.find_one({'_id': ObjectId(id)})
-        return user
-
-    @classmethod
-    async def get_by_username(cls, username: str):
+    async def get_by_username(cls, username: str) -> dict:
         user = await cls.collection.find_one({'username': username})
         return user
 
-    @classmethod
-    async def get_users(cls):
-        users = []
-        cursor = cls.collection.find({})
-        async for doc in cursor:
-            users.append(User(**doc))
-        return users
+class QuestionController(BaseController):
+    model = Question
+    collection = database['questions']
 
     @classmethod
-    async def create(cls, user):
-        new_user = await cls.collection.insert_one(user)
-        created_user = await cls.collection.find_one({'_id': new_user.inserted_id})
-        created_user['_id'] = str(created_user['_id'])
-        return created_user
+    async def get_answers(cls) -> List[Answer]:
+        pass
+
+class AnswerController(BaseController):
+    model = Answer
+    collection = database['answers']
+
+class TagController(BaseController):
+    model = Tag
+    collection = database['tags']
 
     @classmethod
-    async def update(cls, id: str, user):
-        await cls.collection.update_one({'_id': id}, {'$set': user})
-        document = await cls.collection.find_one({'_id': id})
-        return document 
-
-    @classmethod
-    async def delete(cls, id: str):
-        await cls.collection.delete_one({'_id': id})
-        return True 
-
-
+    async def get_questions(cls) -> List[Question]:
+        pass
--- api/app/models.py
+++ api/app/models.py
@@ -1,8 +1,33 @@
 # Global models
-from pydantic import BaseModel, EmailStr 
+from typing import List
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr, Field
 
 class User(BaseModel):
     username: str
     email: EmailStr
     password: str
+    created_at: datetime = Field(default_factory=datetime.now)
     is_active: bool = True
+
+class Question(BaseModel):
+    title: str
+    content: str
+    votes: int
+    tags: List[str]
+    author_id: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = None
+    is_archived: bool = False
+
+class Tag(BaseModel):
+    name: str
+    description: str
+    related_questions: List[str]
+
+class Answer(BaseModel):
+    content: str
+    question_id: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = None
